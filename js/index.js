0d12f1aeb8c4510c69d3789b66e15691d5a7f5b2
Olga Vlasova <73309221+OlgaVlasova-IT@users.noreply.github.com>
2023-09-20 17:10:44 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// gsap.from("#header", {opacity: 0, duration: 3, stagger: 0.2});
+//
+//gsap.from(".imgGaleryIndex", {x: -200, ease: "back", duration: 2})
+gsap.from("#header", {y:60, duration: 2, stagger: 0.2 , opacity: 0})
+gsap.from("#quote", {y:60,  duration: 1.5, stagger: 0.2 , delay: 0.5, opacity: 0})
+gsap.from("#portfolioLink", {y:60,  duration: 1.5, stagger: 0.2 , delay: 0.5, opacity: 0})
