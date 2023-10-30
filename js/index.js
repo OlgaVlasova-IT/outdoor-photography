a451c9b48471637faa39a691332207bf0ef6ec48
Olga Vlasova <73309221+OlgaVlasova-IT@users.noreply.github.com>
2023-10-30 17:00:28 -0600

    2023-10-29 5:00pm

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // gsap.from("#header", {opacity: 0, duration: 3, stagger: 0.2});
 //
 //gsap.from(".imgGaleryIndex", {x: -200, ease: "back", duration: 2})
-gsap.from("#header", {y:120, duration: 2, stagger: 1 , opacity: 0})
-gsap.from("#quote", {y:60,  duration: 1.5, stagger: 0.2 , delay: 0.5, opacity: 0})
-gsap.from("#portfolioLink", {y:60,  duration: 1.5, stagger: 0.2 , delay: 0.5, opacity: 0})
+gsap.from("#header", {x:120, duration: 2, stagger: 1 , opacity: 0})
+// gsap.from("#quote", {x:60,  duration: 1.5, stagger: 0.2 , delay: 0.5, opacity: 0})
+gsap.from("#portfolioLink", {x:-120,  duration: 1.5, stagger: 0.2 , delay: 0.5, opacity: 0})
